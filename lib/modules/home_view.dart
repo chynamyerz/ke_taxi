87cd97e437c9bc1e3dbe8eae212f02f8400d1dbb
swluthuli <swluthuli@gmail.com>
2019-12-18 14:23:57 +0200

    first commit

--- lib/modules/home_view.dart
+++ lib/modules/home_view.dart
@@ -1,16 +1,96 @@
 import 'package:flutter/material.dart';
 import 'package:ke_taxi/widgets/drawer.dart';
 
-class HomeView extends StatelessWidget {
-  static const String routeName = '/home';
+class HomeView extends StatefulWidget {
+   static const String routeName = '/home';
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
 
+class _HomeViewState extends State<HomeView> {
+    int _selectedFromLocation;
+  int _selectedToLocation;
+  
+  List<DropdownMenuItem<int>> locationList = [];
+
+  void loadGenderList() {
+    locationList = [];
+    locationList.add(new DropdownMenuItem(
+      child: new Text('Taxi Rank'),
+      value: 0,
+    ));
+    locationList.add(new DropdownMenuItem(
+      child: new Text('Down Town'),
+      value: 1,
+    ));
+    locationList.add(new DropdownMenuItem(
+      child: new Text('Ekasi'),
+      value: 2,
+    ));
+  }
   @override
   Widget build(BuildContext context) {
+    loadGenderList();
     return new Scaffold(
         appBar: AppBar(
           title: Text("Home"),
         ),
         drawer: AppDrawer(),
-        body: Center(child: Text("Home")));
+        body:  Card( child: 
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+       
+       ListTile(
+            leading: Icon(Icons.album),
+            title: Column(
+             children: getFormWidget(),
+             
+            ), 
+            
+          ),
+        //  new ListView(
+        //   children: getFormWidget(),
+        // ),
+      
+        ],),),
+        );
+  }
+   List<Widget> getFormWidget() {
+    List<Widget> formWidget = new List();
+
+  formWidget.add(new DropdownButton(
+      hint: new Text('Please choose location'),
+      items: locationList,
+      value: _selectedFromLocation,
+      onChanged: (value) {
+        setState(() {
+          _selectedFromLocation = value;
+        });
+      },
+      isExpanded: true,
+    ),
+    );
+     formWidget.add(new DropdownButton(
+      hint: new Text('Please choose location'),
+      items: locationList,
+      value: _selectedToLocation,
+      underline: Container(
+      height: 2,
+      color: Colors.white,
+    ),
+      onChanged: (value) {
+        setState(() {
+          _selectedToLocation = value;
+        });
+      },
+      isExpanded: true,
+    ),
+    );
+    
+    return formWidget;
   }
 }
+
+
+
