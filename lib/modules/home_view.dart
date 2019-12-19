d4856dc39984fb0f03b4bee228ff50a1fcfabbdd
swluthuli <swluthuli@gmail.com>
2019-12-19 08:49:17 +0200

    fixed Formatting

--- lib/modules/home_view.dart
+++ lib/modules/home_view.dart
@@ -2,15 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:ke_taxi/widgets/drawer.dart';
 
 class HomeView extends StatefulWidget {
-   static const String routeName = '/home';
+  static const String routeName = '/home';
   @override
   _HomeViewState createState() => _HomeViewState();
 }
 
 class _HomeViewState extends State<HomeView> {
-    int _selectedFromLocation;
+  int _selectedFromLocation;
   int _selectedToLocation;
-  
+
   List<DropdownMenuItem<int>> locationList = [];
 
   void loadGenderList() {
@@ -28,69 +28,187 @@ class _HomeViewState extends State<HomeView> {
       value: 2,
     ));
   }
+
   @override
   Widget build(BuildContext context) {
     loadGenderList();
     return new Scaffold(
-        appBar: AppBar(
-          title: Text("Home"),
-        ),
-        drawer: AppDrawer(),
-        body:  Card( child: 
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-       
-       ListTile(
-            leading: Icon(Icons.album),
-            title: Column(
-             children: getFormWidget(),
-             
-            ), 
-            
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      drawer: AppDrawer(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Card(
+            elevation: 3.0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ListTile(
+                    leading: Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
+                      child: Icon(Icons.place),
+                    ),
+                    title: Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
+                      child: Column(
+                        children: getFormWidget(),
+                      ),
+                    )),
+                //  new ListView(
+                //   children: getFormWidget(),
+                // ),
+              ],
+            ),
           ),
-        //  new ListView(
-        //   children: getFormWidget(),
-        // ),
-      
-        ],),),
-        );
+          Container(
+              width: double.infinity,
+              child: Card(
+                elevation: 3.0,
+                color: Color(0xFF2A2E43),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.fromLTRB(10, 15, 0.0, 25.0),
+                      child: Text(
+                        "Ride Sharing:",
+                        style: TextStyle(color: Colors.white, fontSize: 20.0),
+                        textAlign: TextAlign.start,
+                      ),
+                    ),
+                    Container(
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            width: 130,
+                            height: 100,
+                            child: Card(
+                             color: Color(0xFF353A50),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  IconButton(
+                                    color: Color(0xFFF4911E),
+                                    icon: Icon(Icons.person),
+                                    onPressed: () {
+                                      setState(() {
+                                        /** */
+                                      });
+                                    },
+                                  ),
+                                  Text(
+                                    'Only Me',
+                                    style: TextStyle(color: Colors.white),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: 130,
+                            height: 100,
+                            child: Card(
+                              color: Color(0xFF353A50),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  IconButton(
+                                    color: Color(0xFFF4911E),
+                                    icon: Icon(Icons.group_add),
+                                    onPressed: () {
+                                      setState(() {
+                                        /** */
+                                      });
+                                    },
+                                  ),
+                                  Text(
+                                    'Group',
+                                    style: TextStyle(color: Colors.white),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.fromLTRB(10.0, 25.0, 0.0, 0.0),
+                      child: Text(
+                        'Ride for 5 people',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.fromLTRB(10.0, 20, 10.0, 15.0),
+                      width: double.infinity,
+                      height: 80,
+                      child: RaisedButton(
+                        
+                       color: Color(0xFFF4911E),
+                        onPressed: () {},
+                        child: Text('CALL TAXI',style: TextStyle(color: Colors.white),),
+                      ),
+                    ),
+                  ],
+                ),
+              )),
+        ],
+      ),
+    );
   }
-   List<Widget> getFormWidget() {
+
+  List<Widget> getFormWidget() {
     List<Widget> formWidget = new List();
 
-  formWidget.add(new DropdownButton(
-      hint: new Text('Please choose location'),
-      items: locationList,
-      value: _selectedFromLocation,
-      onChanged: (value) {
-        setState(() {
-          _selectedFromLocation = value;
-        });
-      },
-      isExpanded: true,
-    ),
+    formWidget.add(
+      new DropdownButton(
+        icon: Icon(Icons.keyboard_arrow_down),
+        hint: new Text('From'),
+        items: locationList,
+        value: _selectedFromLocation,
+        underline: Container(
+          height: 2,
+          color: Colors.white,
+        ),
+        onChanged: (value) {
+          setState(() {
+            _selectedFromLocation = value;
+          });
+        },
+        isExpanded: true,
+      ),
     );
-     formWidget.add(new DropdownButton(
-      hint: new Text('Please choose location'),
-      items: locationList,
-      value: _selectedToLocation,
-      underline: Container(
-      height: 2,
-      color: Colors.white,
-    ),
-      onChanged: (value) {
-        setState(() {
-          _selectedToLocation = value;
-        });
-      },
-      isExpanded: true,
-    ),
+
+    formWidget.add(
+      Divider(
+        color: Colors.black,
+        height: 2,
+      ),
     );
-    
+    formWidget.add(
+      new DropdownButton(
+        icon: Icon(Icons.keyboard_arrow_down),
+        hint: new Text('To'),
+        items: locationList,
+        value: _selectedToLocation,
+        underline: Container(
+          height: 2,
+          color: Colors.white,
+        ),
+        onChanged: (value) {
+          setState(() {
+            _selectedToLocation = value;
+          });
+        },
+        isExpanded: true,
+      ),
+    );
+
     return formWidget;
   }
 }
-
-
-
--- lib/modules/signin_view.dart
+++ lib/modules/signin_view.dart
@@ -4,7 +4,7 @@ import 'package:ke_taxi/modules/signup_view.dart';
 
 class SigninView extends StatelessWidget {
   static const String routeName = '/signin';
- final _formKey = GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -13,104 +13,126 @@ class SigninView extends StatelessWidget {
         ),
         drawer: AppDrawer(),
         body: Form(
-    key: _formKey,
-    child: Padding( padding: const EdgeInsets.symmetric(horizontal: 10.0),
-          child: ListView(children: <Widget>[ Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Center(child:Image.asset('assets/images/keTaxi.png',width: 200.0),),
-        Center(child: Text('Welcome Back!',textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold),textScaleFactor: 2.0,),),
-        Center(child: Text('unlock the easy way of public transport',textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.normal),textScaleFactor: 1.0,),),
-       
-        
-        Padding(padding: const EdgeInsets.only(top:25.0,left: 10.0,right: 10.0),
-        child:
-        TextFormField(
-          decoration: const InputDecoration(
-            hintText: 'Email',
-            border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(4.0))),
-            prefixIcon: Icon(Icons.mail,color: Colors.orange),
-             focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.orange), borderRadius: BorderRadius.all(Radius.circular(4.0))),
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: ListView(
+              children: <Widget>[
+                Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Center(
+                        child: Image.asset('assets/images/keTaxi.png',
+                            width: 200.0),
+                      ),
+                      Center(
+                        child: Text(
+                          'Welcome Back!',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                          textScaleFactor: 2.0,
+                        ),
+                      ),
+                      Center(
+                        child: Text(
+                          'unlock the easy way of public transport',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontWeight: FontWeight.normal),
+                          textScaleFactor: 1.0,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 25.0, left: 10.0, right: 10.0),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            hintText: 'Email',
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(4.0))),
+                            prefixIcon: Icon(Icons.mail, color: Colors.orange),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.orange),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(4.0))),
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter a Valid Email';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 25.0, left: 10.0, right: 10.0),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            hintText: 'Password',
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(4.0))),
+                            prefixIcon: Icon(Icons.lock, color: Colors.orange),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.orange),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(4.0))),
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter some text';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        width: double.infinity, // match_parent
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 25.0),
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 10.0),
+                            child: RaisedButton(
+                              color: Colors.orange,
+                              onPressed: () {
+                                // Validate will return true if the form is valid, or false if
+                                // the form is invalid.
+                                // if (_formKey.currentState.validate()) {
+                                //   // Process data.
+                                // }
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SigninView()),
+                                );
+                              },
+                              child: Text('Signin'),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text('Dont have an account? '),
+                          GestureDetector(
+                              child: Text("Signup",
+                                  style: TextStyle(color: Colors.blue)),
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SignupView()),
+                                );
+                              })
+                        ],
+                      ),
+                    ]),
+              ],
+            ),
           ),
-          validator: (value) {
-            if (value.isEmpty) {
-              return 'Please enter a Valid Email';
-            }
-            return null;
-          },
-        ),
-        ),
-       
-        
-        Padding(padding: const EdgeInsets.only(top:25.0,left: 10.0,right: 10.0),
-        child:
-        TextFormField(
-          decoration: const InputDecoration(
-            hintText: 'Password',
-            border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(4.0))),
-            prefixIcon: Icon(Icons.lock,color: Colors.orange),
-             focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.orange), borderRadius: BorderRadius.all(Radius.circular(4.0))),
-          ),
-          validator: (value) {
-            if (value.isEmpty) {
-              return 'Please enter some text';
-            }
-            return null;
-          },
-        ),),
-        SizedBox(
-  width: double.infinity, // match_parent
-  child:  Padding(padding: const EdgeInsets.only(top: 25.0),
-        child:
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10.0),
-          child: RaisedButton(
-            color: Colors.orange,
-            onPressed: () {
-              // Validate will return true if the form is valid, or false if
-              // the form is invalid.
-              // if (_formKey.currentState.validate()) {
-              //   // Process data.
-              // }
-              Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => SigninView()),
-  );
-            },
-            child: Text('Signin'),
-          ),
-        ),
-        ),
-),
-   
-  Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-  children: <Widget>[
-    Text('Dont have an account? '),
-   
-    
-  GestureDetector(
-  child: Text("Signup", style: TextStyle(color: Colors.blue)),
-  onTap: () {
-   Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => SignupView()),
-  );
-  }
-)
-    
-  ],
-),
-
-  ]
-),
-        
-      ],
-    ),
- 
- 
-  ),
-        )
-        );
+        ));
   }
 }
